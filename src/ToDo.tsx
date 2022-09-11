4c82eb71eb1e3068b9c6c4c3f047d79738fecba2
griteach <griteach22@gmail.com>
2022-09-11 23:06:18 +0900

    first

---
+++ src/ToDo.tsx
@@ -0,0 +1,6 @@
+function Todo(){
+    return <h1>TODO</h1>;
+}
+
+
+export default Todo;
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'styled-components';
+
+
+export const lightTheme: DefaultTheme = {
+    bgColor:"whitesmoke",
+    textColor:"black",
+    accentColor:"#8c7ae6",
+}
